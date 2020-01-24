1f4bbe2981fd93fd8b3f3ec9d0a48247e81c42b5
cespenar <cespenar1@gmail.com>
2020-01-24 12:53:51 +0100

    added GyreData class

--- sdb.py
+++ sdb.py
@@ -6,6 +6,104 @@ import os
 import shutil
 
 
+class GyreData(object):
+
+    """Structure containing data from a GYRE output file.
+
+        Assumes the following structure of a file:
+        
+        line 1: blank
+        line 2: header numbers
+        line 3: header names
+        line 4: header data
+        line 5: body numbers
+        line 6: body names
+        lines 7-: body data
+    
+    Ispired by Bill Wolf's PyMesaReader.
+
+    Parameters
+    ----------
+    file_name : str
+        The name of GYRE output file to be read in.
+
+    Attributes
+    ----------
+    file_name : str
+        Path to the GYRE output file.
+    bulk_data : numpy.ndarray
+        The main data in the structured array format.
+    bulk_names : tuple of str
+        List of all available data column names.
+    header_data : dict
+        Header data in dict format.
+    header_names : list of str
+        List of all available header names.
+    """
+
+    header_names_line = 3
+    body_names_line = 6
+
+    @classmethod
+    def set_header_names_line(cls, name_line=2):
+        cls.header_names_line = name_line
+
+    @classmethod
+    def set_body_names_line(cls, name_line=6):
+        cls.bulk_names_line = name_line
+    
+    def __init__(self, file_name):
+        """Make a GyreData object from a GYRE output file.
+
+        Assumes the following structure of a file:
+        
+        line 1: blank
+        line 2: header numbers
+        line 3: header names
+        line 4: header data
+        line 5: body numbers
+        line 6: body names
+        lines 7-: body data
+
+        This structure can be altered by the class methods
+        'GyreData.set_header_names_line' and 'GyreData.set_body_names_line'.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of GYRE output file to be read in.
+        """
+
+        self.file_name = file_name
+        self.header_names = None
+        self.header_data = None
+        self.body_names = None
+        self.body_data = None
+        self.read_gyre()
+
+    def read_gyre(self):
+        """Reads data from the GYRE output file."""
+
+        self.body_data = np.genfromtxt(
+            self.file_name, skip_header=GyreData.body_names_line - 1,
+            names=True, dtype=None)
+        
+        self.body_names = self.body_data.dtype.names
+
+        header_data = []
+        with open(self.file_name) as f:
+            for i, line in enumerate(f):
+                if i == GyreData.header_names_line - 1:
+                    self.header_names = line.split()
+                elif i == GyreData.header_names_line:
+                    header_data = [float(v) for v in line.split()]
+                elif i > GyreData.header_names_line:
+                    break
+        self.header_data = dict(zip(self.header_names, header_data))
+
+
+
+
 def read_progenitor_name(f_name):
     """Recovers initial values from the name of history file that
     follows specific format of progenitors' grid.
@@ -174,24 +272,6 @@ def evaluate_initial_grid(grid_dir, output):
     save_grid_to_file(grid, output)
     
 
-def read_grid_file(f_name):
-    """Reads a grid file. Uses pandas.
-
-    Parameters
-    ----------
-    f_name : str
-        A grid file.
-
-    Returns
-    ----------
-    pandas.core.frame.DataFrame
-        Pandas DataFrame with grid.
-    """
-
-    grid = np.genfromtxt(f_name, dtype=None, names=True)
-    return pd.DataFrame(grid)
-
-
 def read_grid_file_numpy(f_name):
     """Reads a grid file and returns structured numpy arrary.
 
@@ -207,6 +287,23 @@ def read_grid_file_numpy(f_name):
     """
 
     return np.genfromtxt(f_name, dtype=None, names=True)
+
+
+def read_grid_file(f_name):
+    """Reads a grid file. Uses pandas.
+
+    Parameters
+    ----------
+    f_name : str
+        A grid file.
+
+    Returns
+    ----------
+    pandas.core.frame.DataFrame
+        Pandas DataFrame with grid.
+    """
+
+    return pd.DataFrame(read_grid_file_numpy(f_name))
 
 
 def make_replacements(file_in, file_out, replacements, remove_original=False):
